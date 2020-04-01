8d3f1826e6853163d55a4018d6dde9bce379f69f
Alejandro Martino Uria <amartino@izertis.com>
2020-04-01 16:59:54 +0200

    SEMANTMURC-55 send message to general-data topic

--- input-processor-service/src/main/java/es/um/asio/inputprocessor/service/service/impl/DatasetGenericServiceImpl.java
+++ input-processor-service/src/main/java/es/um/asio/inputprocessor/service/service/impl/DatasetGenericServiceImpl.java
@@ -1,6 +1,8 @@
 package es.um.asio.inputprocessor.service.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import es.um.asio.domain.DataSetDataBase;
@@ -14,6 +16,18 @@ import es.um.asio.inputprocessor.service.service.DatasetGenericService;
 @Service
 public class DatasetGenericServiceImpl implements DatasetGenericService {
     
+	/**
+	 * Kafka template.
+	 */
+	@Autowired
+	private KafkaTemplate<String, DataSetDataBase> kafkaTemplate;
+	
+	/**
+	 * Topic name
+	 */
+	@Value("${app.kafka.general-topic-name}")
+	private String topicName;
+	
     /**
      * Dataset generic repository.
      */
@@ -25,6 +39,9 @@ public class DatasetGenericServiceImpl implements DatasetGenericService {
      */
     @Override
     public DataSetDataBase save(DataSetDataBase data) {
+    	
+    	kafkaTemplate.send(topicName, data);
+    	
         return datasetGenericRepository.save(data);
     }
 
