b5cfd22a5f527ed50d45f72889b3b839d8545056
Luis Marcos Rivera <lmarcos@izertis.com>
2020-04-13 14:01:07 +0200

    SEMANTMURC-132 Utilizar el enumerado de ASIO-abstractions

--- input-processor-service/src/test/java/es/um/asio/inputprocessor/service/test/repository/DatasetGenericRepositoryTest.java
+++ input-processor-service/src/test/java/es/um/asio/inputprocessor/service/test/repository/DatasetGenericRepositoryTest.java
@@ -1,5 +1,6 @@
 package es.um.asio.inputprocessor.service.test.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import es.um.asio.abstractions.domain.Operation;
 import es.um.asio.domain.proyectos.Proyecto;
 import es.um.asio.inputprocessor.service.repository.generic.DatasetGenericRepository;
 
@@ -32,4 +34,21 @@ public class DatasetGenericRepositoryTest {
         repository.save(proyecto);
         assertNotNull(proyecto.getEntityId());
     }
+    
+    @Test
+    public void whenSaveNewDatasetWithoutOperation_thenDatasetHasInsertOperation() {
+        Proyecto proyecto = new Proyecto();
+        
+        repository.save(proyecto);
+        assertThat(proyecto.getOperation()).isEqualTo(Operation.INSERT);
+    }
+    
+    @Test
+    public void whenSaveNewDatasetWithDeleteOperation_thenDatasetHasDeleteOperation() {
+        Proyecto proyecto = new Proyecto();
+        proyecto.setOperation(Operation.DELETE);
+        
+        repository.save(proyecto);
+        assertThat(proyecto.getOperation()).isEqualTo(Operation.DELETE);
+    }    
 }
