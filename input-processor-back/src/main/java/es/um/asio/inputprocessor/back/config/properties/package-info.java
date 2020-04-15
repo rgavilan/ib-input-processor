9e0fea65f6cff0a7ca47ded05db452e71441ad33
Luis Marcos Rivera <lmarcos@izertis.com>
2020-04-15 13:26:22 +0200

    SEMANTMURC-115 Guardar fecha de última llamada a servicios CVN y leerlo

---
+++ input-processor-back/src/main/java/es/um/asio/inputprocessor/back/config/properties/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing configuration properties
+ */
+package es.um.asio.inputprocessor.back.config.properties;
---
+++ input-processor-back/src/test/java/es/um/asio/back/test/TestApplication.java
@@ -0,0 +1,22 @@
+package es.um.asio.back.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+//@Import(MapperConfig.class)
+public class TestApplication {
+    /**
+     * Main method for embedded deployment.
+     *
+     * @param args
+     *            the arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
