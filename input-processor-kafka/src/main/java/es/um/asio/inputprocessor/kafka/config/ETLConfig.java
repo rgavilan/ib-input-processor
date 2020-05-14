612ced17f777cebef9d49372fbaeb9be8be436e5
Alejandro Martino Uria <amartino@izertis.com>
2020-05-14 10:44:16 +0200

    SEMANTMURC-100 fix problem parse response from pentaho

--- input-processor-kafka/src/main/java/es/um/asio/inputprocessor/kafka/config/ETLConfig.java
+++ input-processor-kafka/src/main/java/es/um/asio/inputprocessor/kafka/config/ETLConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,6 +18,19 @@ public class ETLConfig {
 	public RestTemplate cvnRestTemplate() {
 
 		RestTemplate restTemplate = new RestTemplate();
+		
+		// MessageConverter xml
+		Jaxb2RootElementHttpMessageConverter jaxb2RootElementHttpMessageConverter = new Jaxb2RootElementHttpMessageConverter();
+		jaxb2RootElementHttpMessageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+						MediaType.APPLICATION_OCTET_STREAM, 
+						MediaType.APPLICATION_XML, 
+						MediaType.TEXT_XML));
+		
+		restTemplate.getMessageConverters().add(jaxb2RootElementHttpMessageConverter);
+		
+
+		// MessageConverter json
 		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
 		mappingJackson2HttpMessageConverter.setSupportedMediaTypes(
 				Arrays.asList(
@@ -24,6 +38,8 @@ public class ETLConfig {
 						MediaType.APPLICATION_OCTET_STREAM, 
 						MediaType.APPLICATION_XML, 
 						MediaType.TEXT_XML));
+		
+		
 		restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
 
 		return restTemplate;
